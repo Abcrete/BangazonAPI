087fcfeea4f30d3d6eef1cc22205696ecceebf42
Jason Smith <jasonlsmith52@gmail.com>
2017-07-25 11:08:01 -0500

    added ERD to docs folder

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId {get; set;}
+        [Required]
+        public string Title {get; set;}
+        [Required]
+        public double Price {get; set;}
+        [Required]
+        public int Quantity {get; set;}
+        [Required]
+        public string Description {get; set;}
+        [Required]
+        public int CustomerId {get; set;}
+        [Required]
+        public int ProductTypeId {get; set;}
+        
+    }
+}
