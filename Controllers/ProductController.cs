29d1fa4d8e09be02177c4ed0d4efa233f035cd5b
Jason Smith <jasonlsmith52@gmail.com>
2017-07-26 10:42:39 -0500

    finished product controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,47 +3,139 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BangazonAPI.Data;
+using BangazonAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace BangazonAPI.Controllers
 {
     [Route("api/[controller]")]
     public class ProductController : Controller
     {
-        //private BangazonContext _context;
-        // public ProductController(BangazonContext ctx)
-        // {
-        //     _context = ctx;
-        // }
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private BangazonContext _context;
+        public ProductController(BangazonContext ctx)
         {
-            return new string[] { "value1", "value2" };
+            _context = ctx;
+        }
+        public IActionResult Get()
+        {
+            IQueryable<object> products = from product in _context.Product select product;
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return Ok(products);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id}", Name = "GetProduct")]
+        public IActionResult Get(int id)
         {
-            return "value";
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Product product = _context.Product.Single(p => p.ProductId == id);
+                if(product == null){ 
+                    return NotFound();
+                }
+                return Ok(product);
+            }
+            catch (System.InvalidOperationException)
+            {
+                return NotFound();
+            }
         }
 
-        // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody] Product product)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Product.Add(product);
+            
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductExists(product.ProductId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("GetProduct", new { id = product.ProductId }, product);
         }
 
-        // PUT api/values/5
+        public bool ProductExists(int id)
+        {
+            return _context.Product.Count(e => e.ProductId == id) > 0;
+        }
+
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody] Product product)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
         }
 
-        // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Product product = _context.Product.Single(p => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Product.Remove(product);
+            _context.SaveChanges();
+
+            return Ok(product);
         }
     }
 }
