b7cbf90b0b8fae613cb193a6e0ca57226c2655f4
Tamela Lerma <tlerma83@gmail.com>
2017-07-27 15:53:12 -0500

    more docs on computer class and controller, started on employee class

--- Controllers/ComputerController.cs
+++ Controllers/ComputerController.cs
@@ -13,15 +13,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BangazonAPI.Controllers
 {
-    // <summary>
     // Route sets the path the server will be listening for
     // EnableCors sets the allowed sites that can request data using the methods established in this Controller
+    // Authored by: Tamela Lerma
     [Route ("api/[controller]")]
     [EnableCors("AllowSpecificOrigin")]
     public class ComputerController : Controller
     {
-        // <summary>
-        // Property _context sets public constructer to make requests and set data
+        // Private Property _context sets public constructer to make requests and set data
+        // Authored by: Tamela Lerma
         private BangazonContext _context;
 
         public ComputerController (BangazonContext ctx) 
@@ -29,8 +29,8 @@ namespace BangazonAPI.Controllers
             _context = ctx;
         }
 
-        // <summary>
-        // Overloaded Method that can return a complete Computer list or a single computer 
+        // Method that can return all Computer objects. This Method does not take any Arguements
+        // Authored by: Tamela Lerma
         [HttpGet]
         public IActionResult Get()
         {
@@ -39,6 +39,7 @@ namespace BangazonAPI.Controllers
             // This also allows iteration
             // <values>
             // Returns an object for each instance
+            // Authored by: Tamela Lerma
             IQueryable<object> computer = from comp in _context.Computer select comp;
             if (computer == null)
             {
@@ -49,6 +50,9 @@ namespace BangazonAPI.Controllers
 
         }
 
+        // Over-Loaded Method that can return a single Computer Object.
+        // Arguments : This Method takes 1 argument of ComputerId
+        // Authored by: Tamela Lerma
         [HttpGet("{id}", Name = "GetComputer")]
         public IActionResult Get(int id)
         {
@@ -73,8 +77,12 @@ namespace BangazonAPI.Controllers
             }
         }
 
-        // <summary>
-        // post api/value, requires all columns in table to be applied in action
+        // Post path is api/computer, Method requires all columns in table
+        // ComputerId is Auto-Generated, value does not have to be applied when creating
+        // Required-
+        // DateDecomissioned : accepts standard IOS Date and Time Format, ex. "yyyy-MM-dd 'at' HH:mm"
+        // DatePurchased : accepts standard IOS Date and Time Format, ex. "yyyy-MM-dd 'at' HH:mm"
+        // Authored by: Tamela Lerma
         [HttpPost]
         public  IActionResult Post([FromBody] Computer computer)
         {
@@ -86,6 +94,7 @@ namespace BangazonAPI.Controllers
             _context.Computer.Add(computer);
             // <summary>
             //If no error will save changes to DB
+            // Authored by: Tamela Lerma
             try
             {
                 _context.SaveChanges();
@@ -104,18 +113,29 @@ namespace BangazonAPI.Controllers
             }
             // <value>
             // returns new object instance
+            // Authored by: Tamela Lerma
             return CreatedAtRoute("GetComputer", new {id = computer.ComputerId}, computer);
         }
 
-        // <value>
-        // returns True if ComputerId exists
+        // internal Method to check if ComputerId exists, 
+        // if Count is greater than 0 returns True
+        // ensures duplicates are not made and correct error is given
+        // Authored by: Tamela Lerma
         private bool ComputerExists(int ComputerID)
         {
             return _context.Computer.Count(c => c.ComputerId == ComputerID) > 0;
         }
 
-        // <summary>
+        // Put Method,  path is api/computer
+        // Method requires all columns in table
         // allows table values to be updated
+        // Required -
+        // ComputerId : Must be ID in Table
+        // DateDecomissioned : must be in "yyyy-MM-dd 'at' HH:mm" format
+        // DatePurchased : mmust be in "yyyy-MM-dd 'at' HH:mm" format
+        // Arguments-
+        // Excepts ComputerId as an Arguments
+        // Authored by: Tamela Lerma
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] Computer computer)
         {
@@ -134,6 +154,9 @@ namespace BangazonAPI.Controllers
 
             try
             {
+                // if ModelState is Valid and if Computer does exist
+                // chnges will be saved to DB
+                // Authored by: Tamela Lerma
                 _context.SaveChanges();
             }
             catch(DbUpdateConcurrencyException)
@@ -147,11 +170,14 @@ namespace BangazonAPI.Controllers
                     throw;
                 }
             }
+            // returns Status of 204 No Content after successful Update
+            // Authored by: Tamela Lerma
             return new StatusCodeResult(StatusCodes.Status204NoContent);
         }
 
-        // <summary>
-        // delete computer api/value/id, no value needed to pass in testing. Apply ComputerId in URI
+        // Delete computer Method api/computer/id, no value needed to pass in testing. Apply ComputerId in URI
+        // Arguments - accepts ComputerId as Argument
+        // Authored by: Tamela Lerma
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
@@ -166,11 +192,10 @@ namespace BangazonAPI.Controllers
             {
                 return NotFound();
             }
-            // <value>
             // removes object from DB if no error occurs
+            // Authored by: Tamela Lerma
             _context.Computer.Remove(computer);
             _context.SaveChanges();
-            // <value>
             // returns Ok on success
             return Ok(computer);
         }
