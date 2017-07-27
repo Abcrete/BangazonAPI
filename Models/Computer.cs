4656c24ab344076f6eac0220a30619c84301cc1a
Tamela Lerma <tlerma83@gmail.com>
2017-07-27 13:24:47 -0500

    added documentation for Computer Model

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+// <summary> 
+// Use Models in namespace for reference to Computer.cs location
 namespace BangazonAPI.Models
 {
     public class Computer
     {
+        // <summary>
+        // Key will auto-increment new entries for ComputerId, ComputerId allows get and set of the ID
         [Key]
         public int ComputerId {get; set;}
 
+        // <summary>
+        // SqLite stores as string, to testing requests formating uses standard IOS DateTime format, ex. "yyyy-MM-dd 'at' HH:mm"
         [Required]
         [DataType(DataType.Date)]
         public DateTime DatePurchased {get; set;}
 
+
+        // <summary>
+        // SqLite stores as string, to testing requests formating uses standard IOS DateTime format, ex. "yyyy-MM-dd 'at' HH:mm"
         [Required]
         [DataType(DataType.Date)]
         public DateTime? DateDecomissioned {get; set;}
